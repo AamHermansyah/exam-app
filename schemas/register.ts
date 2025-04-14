6fd24688d69372777cba9c086eac424cd655fe16
Aam Hermansyah <amzhermanzyah@gmail.com>
2025-04-14 18:11:08 +0700

    [FIX] Error in register schema

--- schemas/register.ts
+++ schemas/register.ts
@@ -5,7 +5,7 @@ export const registerSchema = z.object({
   email: z.string().email({ message: "Email tidak valid" }),
   purpose: z.string().min(5, { message: "Tujuan harus diisi dengan jelas" }),
   institution: z.string().min(2, { message: "Instansi tidak valid" }),
-  password: z.string().min(17, { message: "Password minimal 10 karakter" }),
+  password: z.string().min(6, { message: "Password minimal 6 karakter" }),
 });
 
 export type RegisterSchemaType = z.infer<typeof registerSchema>;
